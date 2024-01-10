fdf5abb732488cf8cb137c33a5370c1789465ebf
SukkaBot <githubbot@skk.moe>
2024-01-10 15:10:33 +0000

    deploy: SukkaW/Surge@611dc7183a46c3247cd44d67f06aa0824127a70f

---
+++ List/non_ip/cloudmounter.ts
@@ -0,0 +1,17 @@
+#########################################
+# 
+# Last Updated: 2024-01-10T15:10:24.195Z
+# Size: 8
+# License: AGPL 3.0
+# Homepage: https://ruleset.skk.moe
+# GitHub: https://github.com/SukkaW/Surge
+#########################################
+export const DOMAINS = [
+'DOMAIN-SUFFIX,sharepoint.com',
+'DOMAIN,www.googleapis.com'
+];
+export const PROCESS_NAMES = [
+'CloudMounter'
+// 'RaiDrive'
+];
+################## EOF ##################
