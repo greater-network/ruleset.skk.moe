bf2de700d6dd2287fa6010b42ae72e65a8b6906b
SukkaBot <githubbot@skk.moe>
2024-02-12 12:28:19 +0000

    deploy: SukkaW/Surge@0523cf055d7e11f52102c8198761552b69bddb02

--- List/internal/appprofile.php
+++ List/internal/appprofile.php
@@ -1,6 +1,6 @@
 <?php
 
-// # Build 2024-02-12T07:00:58.264Z
+// # Build 2024-02-12T12:28:09.263Z
 
 declare(strict_types=1);
 
@@ -2116,6 +2116,7 @@ $_ENV['Clash_Group_Config'] = [
         'IP-CIDR,52.82.190.0/24,Domestic',
         'IP-CIDR,52.82.192.0/18,Domestic',
         'IP-CIDR,52.83.0.0/16,Domestic',
+        'IP-CIDR,52.130.12.0/24,Domestic',
         'IP-CIDR,54.222.0.0/19,Domestic',
         'IP-CIDR,54.222.32.0/21,Domestic',
         'IP-CIDR,54.222.45.0/24,Domestic',
